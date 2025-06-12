b4040402cd2da8590da47c967380b4083acb9a88
Samiya Fyffe <sfyffe@cisco.com>
2025-06-12 14:53:07 -0400

    Pulled updated Diag_Analyzer_v1_05.py from sydneys-edits branch

--- Diag_Analyzer_v1_05.py
+++ Diag_Analyzer_v1_05.py
@@ -9,7 +9,6 @@ import argparse
 '''
 Diag_analyzer.exe v1.04
 22 June 2022
-Edited: 27 May 2025
 
 Usage:
     Diag_analyzer.exe
@@ -29,17 +28,13 @@ It will then create a directory with the diagnostic file name and store the log 
 Next, it will parse the logs and determine the Top 10 Processes, Files, Extensions and Paths and
 print that information to the screen and also to a {Diagnostic}-summary.txt file.
 
-*Note: Added improved functionality:
-- Combined .7z and .zip file handling into unified processing
-- Enhanced version detection and comparison
-- Improved path handling for cross-platform compatibility
-- Added clearer variable naming for maintainability
-*
+Note: 
+Simplified the regex to match the actual Event::HandleCreation log line format.
+Removed redundant code and improved readability.
+Removed ouput as an unnecessary argument in the print functions.
 
 Written by Matthew Franks and Brandon Macer
-Edited by Samiya Fyffe and Sydney Jackson
 '''
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--time",
@@ -51,58 +46,44 @@ args = parser.parse_args()
 
 
 def get_source():
-    """
-    Chooses Diagnostic file to process.
-    Don't need to assign source to a variable, just return the path.
-    Combined .7s and .zip files into one function.
-    """
     if args.infile:
-        #source = os.path.join(os.curdir, args.infile)
-        #return source
-        return os.path.join(os.curdir, args.infile)
+        source = os.path.join(os.curdir, args.infile)
+        return source
     elif args.directory:
-        #source = args.directory
-        return args.directory
+        source = args.directory
+        return source
     else:
         for file in os.listdir(os.curdir):
-            if file.endswith((".7z", ".zip")):
+            if file.endswith(".7z"):
+                source = os.path.join(os.curdir, file)
+                return source
+            elif file.endswith(".zip"):
                 source = os.path.join(os.curdir, file)
                 return source
         exit("No diagnostic file found or specified.")
 
+
 def get_max_version(list_of_paths):
-    """
-    Locates and returns the latest version number from list of paths.
-    Cleaned up integer list with version_parts for easier readability.
-    """
     max_version = [0, 0, 0]
     r = r'\d{1,2}\.\d{1,2}\.\d{1,2}.\d{1,5}'
     for path in list_of_paths:
         reg = re.findall(r, path)
         if reg:
-            version_parts = list(map(lambda x: int(x), reg[0].split(".")))
-            if version_parts > max_version:
-                max_version = version_parts
+            if list(map(lambda x: int(x), reg[0].split("."))) > max_version:
+                max_version = list(map(lambda x: int(x), reg[0].split(".")))
     print("Found latest version: " + ".".join(list(map(lambda x: str(x), max_version))) + "\n")
     return ".".join(list(map(lambda x: str(x), max_version)))
 
+
 def get_version(path):
-    """
-    Extracts and returns version number from file path containing 'sfc.exe'.
-    Returns "0.0.0" if no version is found.
-    Changed variable names for clarity.
-    """
-    regex = r'(\d{1,2}\.\d{1,2}\.\d{1,2}.\d{1,5}).*sfc\.exe.*'
-    match = re.findall(regex, path)
-    if match:
-        return match[0]
+    r = r'(\d{1,2}\.\d{1,2}\.\d{1,2}.\d{1,5}).*sfc\.exe.*'
+    reg = re.findall(r, path)
+    if reg:
+        return reg[0]
     return "0.0.0"
 
+
 def get_log_files_directory(source, output):
-    """
-    Scans the source directory for .7z or .zip files and extracts the log files.
-    Returns the log files from the first archive found.
-    """
     files_7z = []
     for file in os.listdir(source):
         if file.endswith(".7z"):
@@ -114,10 +95,10 @@ def get_log_files_directory(source, output):
         log_files.append(get_log_files(file, output))
     return log_files[0]
 
-
+#Creates the output directory dependent upon version number
 def get_log_files(source, output):
     print("Moving log files into the output directory.\n")
-    with zipfile.ZipFile(source) as archive:
+    with zipfile.ZipFile(source) as archive: 
         namelist = []
         for x in archive.namelist():
             namelist.append(x)
@@ -125,19 +106,20 @@ def get_log_files(source, output):
         for f in archive.namelist():
             if get_version(f) == max_version:
                 fname = os.path.basename(f)
-                source = archive.open(f)
-                target = open(os.path.join(output, fname), "wb")
-                with source, target:
-                    shutil.copyfileobj(source, target)
+                source = archive.open(f) 
+                target = open(os.path.join(output, fname), "wb") 
+                with source, target: 
+                    shutil.copyfileobj(source, target) 
     archive.close()
-    return os.listdir(output)
+    #Returns a list of file names that were just extracted to the output directory
+    return os.listdir(output) 
 
-
-def print_info(data, name, source, output, count=100000):
-    if args.directory:
+# Formats the output.
+def print_info(data, name, source, count=100000):
+    if args.directory: 
         filename = 'Directory-summary.txt'
-    else:
-        filename = f'{source.split(".")[0]}-summary.txt'
+    else: 
+        filename = f'{source.split(".")[0]}-summary.txt' 
     with open(filename, "a") as f:
         print("\n-----------------------------------\nTop {} {}:\n".format(count, name))
         f.write("Top {} {}:\n".format(count, name))
@@ -148,27 +130,29 @@ def print_info(data, name, source, output, count=100000):
         print("\n\n")
         f.write("\n\n")
 
-
-def print_info_to_file(data, name, source, output):
-    if args.directory:
-        file_name = 'Directory-summary.txt'
-    else:
-        file_name = f'{source.split(".")[0]}-summary.txt'
+# Formats and writes the output to a specified file. 
+def print_info_to_file(data, name, results_dir):
+    # Always write to results/summary.txt
+    file_name = os.path.join(results_dir, '-summary.txt')
     with open(file_name, "a") as f:
-        f.write("All {}:\n".format(name))
+        f.write("{}:\n".format(name))
         for i in data:
-            output = '{0:>8}'.format(i[1]), i[0].rstrip()
-            f.write("{} {}\n".format(str(output[0]), str(output[1])))
+            output_line = '{0:>8}'.format(i[1]), i[0].rstrip()
+            f.write("{} {}\n".format(str(output_line[0]), str(output_line[1])))
         f.write("\n\n")
 
 
 def main():
-    try:
-        source = get_source()
-    except AttributeError:
-        exit("No Diagnostic file found.")
-    output = os.path.join(os.getcwd(), source.split('.')[0])
-    print("\nCreating directory\n")
+    source = get_source()
+    output_dir_name = os.path.splitext(os.path.basename(source))[0]
+    output = os.path.join(os.getcwd(), output_dir_name)
+
+    # Create results directory if it doesn't exist
+    results_dir = os.path.join(os.getcwd(), "results")
+    if not os.path.exists(results_dir):
+        os.mkdir(results_dir)
+
+    print("\nCreating directory for extracted logs\n")
     try:
         if not os.path.exists(output):
             os.mkdir(output)
@@ -177,47 +161,39 @@ def main():
             print(f"{output} directory already exists.\n")
     except OSError:
         exit("Creation of the directory {} has failed.\n".format(output))
+
     if args.directory:
         log_files = get_log_files_directory(args.directory, output)
     else:
         log_files = get_log_files(source, output)
     print("Parsing the logs.\n")
-    # Put sfc.exe.log at the end of the list to maintain chronological order
+
+    # Clear the summary file before writing
+    summary_file = os.path.join(results_dir, '-summary.txt')
+    open(summary_file, "w").close()
 
     data = []
     for log in log_files:
         if os.path.isdir(os.path.join(output, log)):
             continue  # Skip directories
-        # Updated regex to match actual Event::HandleCreation log line format
-        # Example: (873358593, +0 ms) May 20 12:39:49 [13340]: Event::HandleCreation: START \\?\C:\Users\...\file(\\?\C:\Program Files\...\file), \\?\C:\Program Files\...\process.exe
         r = r'(\w{3} \d{1,2} \d\d:\d\d:\d\d).*Event::HandleCreation: START (\\\\\?\\[^\(]+)\(\\\\\?\\[^\)]+\), (\\\\\?\\[^\s]+)'
-        r_d = r'(\w{3} \d{1,2} \d\d:\d\d:\d\d)'
         with open(os.path.join(output, log), errors="ignore") as f:
             log_read = f.readlines()
         for line in log_read:
             if "Event::HandleCreation" in line:
-                if args.time:
-                    if re.findall(r_d, line)[0] > args.time:
-                        reg = re.findall(r, line)
-                        if reg:
-                            data.append("{},{},{}\n".format(reg[0][0], reg[0][1], reg[0][2]))
-                else:
-                    reg = re.findall(r, line)
-                    if reg:
-                        data.append("{},{},{}\n".format(reg[0][0], reg[0][1], reg[0][2]))
+                reg = re.findall(r, line)
+                if reg:
+                    data.append("{},{},{}\n".format(reg[0][0], reg[0][1], reg[0][2]))
 
-
-    # Get Process information and print to screen and log
-    process_list = list(map(lambda x: x.split(',')[1], data))
+    # Write results to results/summary.txt
+    process_list = list(map(lambda x: x.split(',')[2], data))
     common_process = Counter(process_list).most_common(10)
-    print_info(common_process, "Processes", source, output, 10)
+    print_info_to_file(common_process, "Processes", results_dir)
 
-    # Get File information and print to screen and log
     file_list = list(map(lambda x: x.split(',')[1], data))
     common_files = Counter(file_list).most_common(10)
-    print_info(common_files, "Files", source, output, 10)
+    print_info_to_file(common_files, "Files", results_dir)
 
-    # Get Extension information and print to screen and log
     extension_list = list(map(lambda x: x.split(',')[1], data))
     extension_list_scrubbed = []
     for i in extension_list:
@@ -225,9 +201,8 @@ def main():
             x = i.split('.')[-1]
             extension_list_scrubbed.append(x)
     common_extensions = Counter(extension_list_scrubbed).most_common(10)
-    print_info(common_extensions, "Extensions", source, output, 10)
+    print_info_to_file(common_extensions, "Extensions", results_dir)
 
-    # Get Path information and print to screen and log
     path_list = list(map(lambda x: x.split(',')[1], data))
     path_list_scrubbed = []
     for i in path_list:
@@ -235,17 +210,7 @@ def main():
         path_only_merged = "\\".join(path_only)
         path_list_scrubbed.append(path_only_merged)
     common_paths = Counter(path_list_scrubbed).most_common(100)
-    print_info(common_paths, "Paths", source, output, 100)
-
-    # Print all file scans to summary file
-    all_files = Counter(file_list).most_common(100000)
-    print_info_to_file(all_files, "Files", source, output)
-
-    #Hold screen open until Enter is pressed
-    if args.directory:
-        filename = 'Directory-summary.txt'
-    else:
-        filename = f'{source.split(".")[0]}-summary.txt'
+    print_info_to_file(common_paths, "Paths", results_dir)
 
 if __name__ == '__main__':
     main()
